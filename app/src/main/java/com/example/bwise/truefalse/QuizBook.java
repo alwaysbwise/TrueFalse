d0dedc9ac66d926a8d372f303cda30c67ffff17b
alwaysbwise <33883062+alwaysbwise@users.noreply.github.com>
2018-04-13 17:06:37 -0700

    Initial commit

---
+++ app/src/main/java/com/example/bwise/truefalse/QuizBook.java
@@ -0,0 +1,25 @@
+package com.example.bwise.truefalse;
+
+/**
+ * Created by wisee on 4/13/2018.
+ */
+
+public class QuizBook {
+
+    public static String[] questions = new String []{
+            "Magnets have 2 poles",
+            "The Earth is flat",
+            "Bitcoin is a Ponzi scheme",
+            "Oceans are fresh water",
+            "Olympia is the capitol of Washington",
+            "Ireland has more people than cows",
+            "Denali is the tallest mountain in the United States",
+            "One gram of carbohydrates is = to 7 calories",
+            "You have a 50% chance of getting this question right",
+            "The moon is made of ice",
+    };
+
+    public static boolean[] answers = new boolean[]{
+            true, false, false, false, true, false, true, false, true, false
+    };
+}
